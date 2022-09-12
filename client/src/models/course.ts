61b7f55c506b53104d5730c2075c4696abb4d4a4
Kalle <domare2@hotmail.com>
2022-09-12 23:54:10 +0200

    fetching data, sass, axios

---
+++ client/src/models/course.ts
@@ -0,0 +1,10 @@
+export interface Course {
+    
+    id: string;
+    title: string;
+    price: number;
+    instructor: string;
+    image: string;
+    rating: number;
+
+  }
---
+++ client/src/pages/Homepage.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Courses from "../components/Courses";
+
+const Homepage = () => {
+    return <h1>
+      <div>
+        <Courses/>
+      </div>
+    </h1>;
+  };
+  export default Homepage;
