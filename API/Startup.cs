830e6aed892d51bbcfffda9d4c2471e1d6efbed4
Kalle <domare2@hotmail.com>
2022-09-16 13:56:20 +0200

    creation of exception middleware

--- API/Startup.cs
+++ API/Startup.cs
@@ -1,4 +1,5 @@
 using API.Helpers;
+using API.Middleware;
 using Entity.Interfaces;
 using Infrastructure;
 using Microsoft.AspNetCore.Builder;
@@ -50,12 +51,14 @@ namespace API
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UseMiddleware<ExceptionMiddleware>();
             if (env.IsDevelopment())
             {
-                app.UseDeveloperExceptionPage();
                 app.UseSwagger();
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "API v1"));
             }
+
+            app.UseStatusCodePagesWithReExecute("/redirect/{0}");
 
             // app.UseHttpsRedirection();
 
