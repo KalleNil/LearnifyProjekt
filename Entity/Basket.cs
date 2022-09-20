56703ff22431898d13c63ab1279353ecfac4e33f
Kalle <domare2@hotmail.com>
2022-09-21 01:18:44 +0200

    basket controller

--- Entity/Basket.cs
+++ Entity/Basket.cs
@@ -7,20 +7,18 @@ namespace Entity
     public class Basket
     {
         public int Id { get; set; }
-
         public string ClientId { get; set; }
-
         public List<BasketItem> Items { get; set; } = new List<BasketItem>();
 
         public void AddCourseItem(Course course)
         {
-            if(Items.All(item => item.CourseId != course.Id))
+            if (Items.All(item => item.CourseId != course.Id))
             {
-                    Items.Add(new BasketItem{Course = course});
+                Items.Add(new BasketItem { Course = course });
             }
         }
 
-        public void RemoveCourseItem(Guid courseId)
+        public void RemoveCourse(Guid courseId)
         {
             var course = Items.FirstOrDefault(item => item.CourseId == courseId);
             Items.Remove(course);
