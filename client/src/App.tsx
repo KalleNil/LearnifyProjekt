bc6469422823f9320cb188809390250b22e8bae5
Kalle <domare2@hotmail.com>
2022-09-23 21:43:25 +0200

    basket slice

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -8,33 +8,21 @@ import Login from './pages/Login';
 import DetailPage from './pages/DetailPage';
 import Categories from './components/Categories';
 import CategoryPage from './pages/CategoryPage';
-import DescriptionPage from './pages/DescriptionPage';
 import BasketPage from './pages/BasketPage';
-import agent from './actions/agent';
 import { useAppDispatch } from './redux/store/configureStore';
-import { setBasket } from './redux/slice/basketSlice';
-import Dashboard from './components/Dashboard';
+import { fetchBasketAsync } from './redux/slice/basketSlice';
 import { getUser } from './redux/slice/userSlice';
+import Dashboard from './components/Dashboard';
+import DescriptionPage from './pages/DescriptionPage';
 
 function App() {
   const dispatch = useAppDispatch();
 
-  function getCookie(name: string) {
-    return (
-      document.cookie.match('(^|;)\\s*' + name + '\\s*=\\s*([^;]+)')?.pop() ||
-      ''
-    );
-  }
-
   useEffect(() => {
-    const clientId = getCookie('clientId');
-    if (clientId) {
-      agent.Baskets.get()
-        .then((basket) => dispatch(setBasket(basket)))
-        .catch((error) => console.log(error));
-    }
+    dispatch(fetchBasketAsync());
     dispatch(getUser());
   }, [dispatch]);
+
   return (
     <>
       <Navigation />
