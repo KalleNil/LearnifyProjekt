4dd677badab101610d40f451fe2954f9a0afcf43
Kalle <domare2@hotmail.com>
2022-09-28 03:06:13 +0200

    Lectures Controller

--- API/Helpers/MappingProfiles.cs
+++ API/Helpers/MappingProfiles.cs
@@ -1,7 +1,4 @@
-using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using API.Dto;
 using AutoMapper;
 using Entity;
@@ -12,26 +9,21 @@ namespace API.Helpers
     {
         public MappingProfiles()
         {
+            
             CreateMap<Course, CourseDto>()
             .ForMember(c => c.Category, o => o.MapFrom(s => s.Category.Name));
-
             CreateMap<Learning, LearningDto>();
-
             CreateMap<Requirement, RequirementDto>();
-
-            CreateMap<Category, CategoriesDto>();
-
-            CreateMap<Category, CategoryDto>();
-
             CreateMap<Basket, BasketDto>();
-
             CreateMap<BasketItem, BasketItemDto>()
             .ForMember(b => b.CourseId, o => o.MapFrom(c => c.Course.Id))
             .ForMember(b => b.Title, o => o.MapFrom(c => c.Course.Title))
             .ForMember(b => b.Price, o => o.MapFrom(c => c.Course.Price))
             .ForMember(b => b.Image, o => o.MapFrom(c => c.Course.Image))
             .ForMember(b => b.Instructor, o => o.MapFrom(c => c.Course.Instructor));
-
-        }
+            CreateMap<Section, SectionDto>()
+            .ForMember(s => s.SectionName, o => o.MapFrom(c => c.Name));
+            CreateMap<Lecture, LectureDto>();
+            }
     }
 }
---
+++ Infrastructure/Migrations/20220928005733_CurrentLectureAdded.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Infrastructure.Migrations
+{
+    public partial class CurrentLectureAdded : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "8d3cf37e-5c58-4998-97c5-860e213bdc79");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "d20a9a77-ba41-4ad2-844a-fd88fe279ceb");
+
+            migrationBuilder.AddColumn<int>(
+                name: "CurrentLecture",
+                table: "UserCourses",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[] { "09c869d6-f83f-4009-9619-70b3446e81bd", "00accdea-3d7d-4e85-bb31-ac5ebb246807", "Student", "STUDENT" });
+
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[] { "59186202-a0f1-4261-b3d0-5102fe547441", "0f5261c9-4675-4b56-8d1a-4e85e20744c1", "Instructor", "INSTRUCTOR" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "09c869d6-f83f-4009-9619-70b3446e81bd");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "59186202-a0f1-4261-b3d0-5102fe547441");
+
+            migrationBuilder.DropColumn(
+                name: "CurrentLecture",
+                table: "UserCourses");
+
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[] { "d20a9a77-ba41-4ad2-844a-fd88fe279ceb", "aa9f12df-01e6-4cf6-8b70-e875dbe71ed6", "Student", "STUDENT" });
+
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[] { "8d3cf37e-5c58-4998-97c5-860e213bdc79", "7fb2d702-f98b-4aba-8183-cf2bee97ad19", "Instructor", "INSTRUCTOR" });
+        }
+    }
+}
